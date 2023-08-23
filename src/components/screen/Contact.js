b2f28472cb67b7bac8d160f5a66be2cba1fc401c
AlfrinP <alfrinpoulose@gmail.com>
2023-08-23 20:48:00 +0530

    added styled to message component

--- src/components/screen/Contact.js
+++ src/components/screen/Contact.js
@@ -22,32 +22,134 @@ export default function Contact() {
       heading: "Files",
       subheading: "12 files",
       view: "view all",
-      content: (
-        <Div style={{width:'100px', height:'50px'}}>
-          <Div style={{width:'100%'}}>
-            <Img src={docs}/>
-            <Span>Brief Project Real Es...</Span>
+      content: [
+        <Div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <Div
+            style={{
+              background: "#242c2e",
+              padding: "5px",
+              borderRadius: "10px",
+            }}
+          >
+            <Div style={{ width: "100%" }} className="box">
+              <Img
+                src={docs}
+                style={{ width: "20px", background: "inherit" }}
+              />
+              <Span
+                style={{
+                  fontSize: "0.75rem",
+                  width: "100%",
+                  background: "inherit",
+                }}
+              >
+                Brief Project Real Es...
+              </Span>
+            </Div>
+            <Div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                background: "inherit",
+                fontSize: "0.5rem",
+                marginTop: "5px",
+              }}
+            >
+              <Span>DOCS</Span>
+              <Span>32KB</Span>
+            </Div>
           </Div>
-          <Div>
-            <Span>DOCS</Span>
-            <Span>32KB</Span>
+          <Span
+            style={{
+              textAlign: "right",
+              width: "100%",
+              fontSize: "0.65rem",
+              marginTop: "2px",
+            }}
+          >
+            12:45 PM
+          </Span>
+        </Div>,
+
+        <Div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <Div
+            style={{
+              background: "#242c2e",
+              padding: "5px",
+              borderRadius: "10px",
+            }}
+          >
+            <Div style={{ width: "100%" }} className="box">
+              <Img
+                src={docs}
+                style={{ width: "20px", background: "inherit" }}
+              />
+              <Span
+                style={{
+                  fontSize: "0.75rem",
+                  width: "100%",
+                  background: "inherit",
+                }}
+              >
+                Brief Project Real Es...
+              </Span>
+            </Div>
+            <Div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                background: "inherit",
+                fontSize: "0.5rem",
+                marginTop: "5px",
+              }}
+            >
+              <Span>DOCS</Span>
+              <Span>32KB</Span>
+            </Div>
           </Div>
-        </Div>
-      ),
+          <Span
+            style={{
+              textAlign: "right",
+              width: "100%",
+              fontSize: "0.65rem",
+              marginTop: "2px",
+            }}
+          >
+            12:45 PM
+          </Span>
+        </Div>,
+      ],
     },
   ];
 
   return (
     <Mdiv>
       <Top>
-        <Span>Contact details</Span>
+        <Span style={{ fontWeight: "600" }}>Contact details</Span>
         <Span className="close">
           <Img src={x} />
         </Span>
       </Top>
+      <hr style={{ width: "100%" }} />
       <Bottom>
         <Div className="mainprofile">
-          <Img src={profile} className="profile" />
+          <Span className="profile">
+            <Img src={profile} className="profile" />
+          </Span>
           <Span className="name">Michael</Span>
           <Span className="status">Online</Span>
         </Div>
@@ -56,10 +158,30 @@ export default function Contact() {
             <React.Fragment key={index}>
               <Div className="contentfirst">
                 <Div className="contenttop">
-                  <Span>{items.heading}</Span>
-                  <Span>{items.subheading}</Span>
+                  <Span style={{ fontWeight: "600", fontSize: "0.85rem" }}>
+                    {items.heading}
+                  </Span>
+                  <Span
+                    style={{
+                      fontSize: "0.65rem",
+                      fontWeight: "600",
+                      color: "#363a3b",
+                    }}
+                  >
+                    {items.subheading}
+                  </Span>
                 </Div>
-                <Span>{items.view}</Span>
+
+                <Span style={{ fontSize: "0.75rem", color: "#48a5c4" }}>
+                  {items.view}
+                  <hr
+                    style={{
+                      color: "#48a5c4",
+                      marginTop: "2px",
+                      width: "100%",
+                    }}
+                  />
+                </Span>
               </Div>
               <Content>{items.content}</Content>
             </React.Fragment>
@@ -71,16 +193,18 @@ export default function Contact() {
 }
 
 const Mdiv = styled.div`
-  height: 100vh;
+  height: 100dvh;
   width: 300px;
   color: white;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  gap: 10px;
 `;
 const Top = styled.div`
   display: flex;
+  width: 100%;
+  height: 65px;
   align-items: center;
   justify-content: space-between;
 `;
@@ -91,6 +215,7 @@ const Div = styled.div`
     align-items: center;
     justify-content: space-between;
     flex-direction: column;
+    gap: 10px;
   }
   &.contentfirst {
     display: flex;
@@ -100,17 +225,41 @@ const Div = styled.div`
   }
   &.contenttop {
     display: flex;
-    gap: 15px;
+    gap: 5px;
     align-items: center;
     justify-content: center;
   }
+  &.box {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    background: #1e2628;
+    padding: 10px;
+    border-radius: 10px;
+  }
 `;
 const Span = styled.span`
+  background: inherit;
   &.online {
     font-size: 1rem;
   }
   &.status {
     font-size: 0.75rem;
+  }
+  &.close {
+    border: 1.5px solid #232a2c;
+    padding: 10px;
+    border-radius: 50%;
+  }
+  &.profile {
+    background: #48a5c4;
+    border-radius: 50%;
+  }
+  &.name {
+    font-weight: 600;
+  }
+  &.status {
+    color: #48a5c4;
   }
 `;
 const Img = styled.img`
@@ -119,7 +268,9 @@ const Img = styled.img`
     border-radius: 10px;
   }
   &.profile {
-    width: 120px;
+    width: 55px;
+    background: inherit;
+    border-radius: 50%;
   }
 `;
 const Content = styled.div`
--- src/components/screen/Message.js
+++ src/components/screen/Message.js
@@ -1,34 +1,43 @@
 import Styled from "styled-components";
 import React from "react";
+import profile from "../../Assets/Property 1=Image 19.png";
+import icon1 from "../../Assets/Property 1=phone.svg";
+import icon2 from "../../Assets/Property 1=video-camera.svg";
+import icon3 from "../../Assets/Option.svg";
 
 export default function Message() {
+  const icons = [icon1, icon2, icon3];
   return (
-    <>
+    <Mdiv>
       <Top>
         <Left>
-          <Img />
-          <Div>
-            <Span></Span>
-            <Span></Span>
+          <Img src={profile} className="profile" />
+          <Div
+            style={{ display: "flex", flexDirection: "column", gap: "5px" }}
+          >
+            <Span style={{fontSize:'1rem',fontWeight:'600'}}>Michael</Span>
+            <Span style={{fontSize:'0.65rem',fontWeight:'600'}}>Typing...</Span>
           </Div>
         </Left>
         <Right>
-          <Ul>
-            <Li></Li>
+          <Ul className="right">
+            {icons.map((icons) => (
+              <Li className="right">
+                <Img src={icons} className="icons" />
+              </Li>
+            ))}
           </Ul>
         </Right>
       </Top>
-      <Mid>
-        <Span>Today</Span>
-      </Mid>
       <Bottom>
+      <Span>Today</Span>
         <Ul>
           <Li>
             <Img />
           </Li>
           <Li>
             <Div>
-              <Input/> <Img/>
+              <Input /> <Img />
             </Div>
           </Li>
           <Li>
@@ -36,18 +45,50 @@ export default function Message() {
           </Li>
         </Ul>
       </Bottom>
-    </>
+    </Mdiv>
   );
 }
-
-const Top = Styled.div``;
-const Mid = Styled.div``;
-const Bottom = Styled.div``;
-const Left = Styled.div``;
+const Mdiv = Styled.div`
+width:550px;
+color:white;
+padding:10px`;
+const Top = Styled.div`
+display:flex;
+align-items:center;
+justify-content:space-between;
+width:100%;
+height:60px;`;
+const Bottom = Styled.div`
+display:flex;
+flex-direction:column;
+align-items:center;
+height:calc(100vh - 60px )
+`;
+const Left = Styled.div`
+display:flex;
+align-items:center;
+justify-content:space-between;`;
 const Right = Styled.div``;
 const Div = Styled.div``;
-const Ul = Styled.ul``;
-const Li = Styled.li``;
+const Ul = Styled.ul`
+&.right{
+  display:flex;
+align-items:center;
+gap:10px;
+}`;
+const Li = Styled.li`
+&.right{
+  border:1.5px solid red;
+  border-radius:50%;
+  padding:10px;
+}`;
 const Span = Styled.span``;
-const Img = Styled.img``;
+const Img = Styled.img`
+background:inherit;
+&.icons{
+  width:20px;
+}
+&.profile{
+  width:55px;
+}`;
 const Input = Styled.input``;
